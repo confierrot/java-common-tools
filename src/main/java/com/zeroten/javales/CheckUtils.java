1c885a52e2f8d6a99746e4590bd8191a50f560dd
yaojl <yaojianliang@745964621@qq.com>
2019-12-17 16:49:59 +0800

    上传作业

--- src/main/java/com/zeroten/javales/CheckUtils.java
+++ src/main/java/com/zeroten/javales/CheckUtils.java
@@ -15,6 +15,27 @@ public class CheckUtils {
         return true;
     }
 
+    public static boolean isEmpty(Object[] arr) {
+        if(arr == null || arr.length==0) {
+            return  true;
+        }
+        return false;
+    }
+
+    public static boolean equals(String str1, String str2) {
+        if(str1 == null || str2 == null){
+            return false;
+        }
+        return str1.equals(str2);
+    }
+    public static boolean equals(Integer n1, Integer n2) {
+        if(n1 == null || n2 == null) {
+            return false;
+        }
+        return n1.equals(n2);
+    }
+
+
     public static void main(String[] args) {
         System.out.println(isAnyEmpty("123", "789"));
     }
--- src/test/java/com/zeroten/javales/CheckUtilsTest.java
+++ src/test/java/com/zeroten/javales/CheckUtilsTest.java
@@ -10,4 +10,23 @@ public class CheckUtilsTest {
         Assert.assertEquals(CheckUtils.isAnyEmpty("123","456"),true);
         Assert.assertEquals(CheckUtils.isAnyEmpty(),false);
     }
+    @Test
+    public void testisEmpty() {
+        Object[] t = new Object[3];
+        Object[] t2 = new Object[0];
+        Assert.assertEquals(CheckUtils.isEmpty(t2), true);
+        Assert.assertEquals(CheckUtils.isEmpty(null),true);
+        Assert.assertEquals(CheckUtils.isEmpty(t),false);
+    }
+
+    @Test
+    public void testEquals() {
+        Assert.assertEquals(CheckUtils.equals("",null), false);
+        Assert.assertEquals(CheckUtils.equals("",""), true);
+        Assert.assertEquals(CheckUtils.equals("12","12"), true);
+
+        Assert.assertEquals(CheckUtils.equals(12,12), true);
+        Assert.assertEquals(CheckUtils.equals(null,12),false);
+        Assert.assertEquals(CheckUtils.equals(11,12),false);
+    }
 }
